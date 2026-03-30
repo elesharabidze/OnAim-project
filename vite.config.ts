365582e28e22630514bba9fae591e64a2b762d24
elesharabidze <elesharabidze@gmail.com>
2026-03-30 14:06:55 +0400

    fix: Vercel deploy errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { getVercelEntries } from 'vite-plugin-vercel'
+import vercel from 'vite-plugin-vercel/vite'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(async () => {
+  const entries = await getVercelEntries('api', { destination: 'api' })
+
+  return {
+    plugins: [
+      react(),
+      vercel({
+        entries,
+        rewrites: [{ source: '/(.*)', destination: '/index.html' }],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3001',
+          changeOrigin: true,
+          rewrite: (p: string) => p.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
